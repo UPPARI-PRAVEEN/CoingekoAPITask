b85bf91c4eeeccce363942dcb814fb5bc2faef1e
Uppari Praveen <upparipraveen2003@gmail.com>
2025-07-12 10:16:26 +0530

    client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Dashboard from './components/dashboard';
+import Dashboard from './components/Dashboard'; 
+
 
 const App = () => {
   return (
